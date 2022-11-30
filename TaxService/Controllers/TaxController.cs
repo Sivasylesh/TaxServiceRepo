9d946d6197987c7f6ee1310d995de16e3641a394
vsiva <vsiva@SIVASYLESH>
2022-11-30 13:56:21 +0530

    Add project files.

---
+++ TaxService/Controllers/TaxController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaxService.Controllers
+{
+    [Route("api/tax")]
+    public class TaxController : ControllerBase
+    {
+        [HttpGet,Route("get")]
+        public IActionResult GetTaxDetails()
+        {
+            return Ok();
+        }
+    }
+}
